8854986fde8932cb08670ebd5a9c95800425e9c8
Geekerui <gaokangrui@gmail.com>
2017-08-03 17:15:16 +0800

    Add Skills

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,82 +1,86 @@
 var bio = {
-    name: "Kangrui",
-    role: "Web Developer",
-    welcomeMessage: "Welcome",
-    bioic: "images/fry.jpg",
-    contacts: {
-        mobile: "138-0000-0000",
-        email: "gaokangrui@gmail.com",
-        github: "Geekerui",
-        twitter: "@gaokangrui",
-        location: "Hefei"
-    },
-    skills: ["awesomeness", "delivering things", "cryogenic sleep", "saving the universe"]
+	name: "Kangrui",
+	role: "Web Developer",
+	welcomeMessage: "Welcome",
+	bioic: "images/fry.jpg",
+	contacts: {
+		mobile: "138-0000-0000",
+		email: "gaokangrui@gmail.com",
+		github: "Geekerui",
+		twitter: "@gaokangrui",
+		location: "Hefei"
+	},
+	skills: ["awesomeness", "delivering things", "cryogenic sleep", "saving the universe"]
 };
 var education = {
-    schools: [{
-        name: "No.50 Middle School",
-        location: "Hefei",
-        degree: "Junior High School",
-        date: "2010-06-30",
-        major: ["CS"],
-        url: "null"
-    }, {
-        name: "Senior High School attached HFUT",
-        location: "Hefei",
-        degree: "Senior High School",
-        date: "2013-06-30",
-        major: ["CS"],
-        url: "null"
-    }, {
-        name: "AHCME",
-        location: "Wuhu",
-        degree: "Colledge",
-        date: "2010-06-30",
-        major: ["CS"],
-        url: "null"
-    }]
+	schools: [{
+		name: "No.50 Middle School",
+		location: "Hefei",
+		degree: "Junior High School",
+		date: "2010-06-30",
+		major: ["CS"],
+		url: "null"
+	}, {
+		name: "Senior High School attached HFUT",
+		location: "Hefei",
+		degree: "Senior High School",
+		date: "2013-06-30",
+		major: ["CS"],
+		url: "null"
+	}, {
+		name: "AHCME",
+		location: "Wuhu",
+		degree: "Colledge",
+		date: "2010-06-30",
+		major: ["CS"],
+		url: "null"
+	}]
 };
 var work = {
-    jobs: [{
-        employer: "null",
-        title: "null",
-        location: "Hefei",
-        dates: "null",
-        description: "null"
-    }, {
-        employer: "null_2",
-        title: "null",
-        location: "Hefei",
-        dates: "null",
-        description: "null"
-    }, {
-        employer: "null_3",
-        title: "null",
-        location: "Hefei",
-        dates: "null",
-        description: "null"
-    }
-    ]
+	jobs: [{
+		employer: "null",
+		title: "null",
+		location: "Hefei",
+		dates: "null",
+		description: "null"
+	}, {
+		employer: "null_2",
+		title: "null",
+		location: "Hefei",
+		dates: "null",
+		description: "null"
+	}, {
+		employer: "null_3",
+		title: "null",
+		location: "Hefei",
+		dates: "null",
+		description: "null"
+	}]
 };
 var projects = {
-    projects: [{
-        title: "null", dates: "null", description: "null", images: ["http://placehold.it/150x150"]
-    }]
+	projects: [{
+		title: "null",
+		dates: "null",
+		description: "null",
+		images: ["http://placehold.it/150x150"]
+	}]
 };
+$("#header").append(HTMLheaderName);
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
 if (bio.skills.length > 0) {
-    $("#header").append(HTMLskillsStart);
-    var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
-    $("#skills").append(formattedSkill);
-    formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
-    $("#skills").append(formattedSkill);
-    formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
-    $("#skills").append(formattedSkill);
-    formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
-    $("#skills").append(formattedSkill);
-    var formattedName = HTMLheaderName.replace("%data%", bio.name);
-    $("#header").prepend(formattedName);
+	$("#header").append(HTMLskillsStart);
+
+	var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
+	$("#skills").append(formattedSkill);
+	formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
+	$("#skills").append(formattedSkill);
+	formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
+	$("#skills").append(formattedSkill);
+	formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
+	$("#skills").append(formattedSkill);
+
 }
-work.jobs.forEach(function (job) {
-    $(".work-entry").append(HTMLworkStart);
-    $("")
-});
+for (job in work.jobs) {
+	$("#workExperience").append(HTMLworkStart);
+
+}
