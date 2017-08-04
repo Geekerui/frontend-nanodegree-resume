1ce8020cc4df6c0453770885411d098ecfa474c6
Geekerui <gaokangrui@gmail.com>
2017-08-04 16:30:27 +0800

    Get the position when click

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -65,23 +65,52 @@ var projects = {
 		images: ["http://placehold.it/150x150"]
 	}]
 };
-$("#header").prepend(HTMLheaderName);
-var formattedName = HTMLheaderName.replace("%data%", bio.name);;
-if (bio.skills.length > 0) {
-	$("#header").append(HTMLskillsStart);
 
-	var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
-	$("#skills").append(formattedSkill);
-	formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
-	$("#skills").append(formattedSkill);
-	formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
-	$("#skills").append(formattedSkill);
-	formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
-	$("#skills").append(formattedSkill);
+// 添加一个函数到代码中用来显示之前的内容
+function displayWork() {
+	// 显示名字
+	var formattedName = HTMLheaderName.replace("%data%", bio.name);
+	$("#header").append(formattedName);
+
+	// 显示技能
+	if (bio.skills.length > 0) {
+		$("#header").append(HTMLskillsStart);
+
+		var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
+		$("#skills").append(formattedSkill);
+		formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
+		$("#skills").append(formattedSkill);
+		formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
+		$("#skills").append(formattedSkill);
+		formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
+		$("#skills").append(formattedSkill);
+	}
+
+	// 显示详情
+	for (job in work.jobs) {
+		$("#workExperience").append(HTMLworkStart);
+
+		var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+		var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+		var formattedEmployerTitle = formattedEmployer + formattedTitle;
+		$(".work-entry:last").append(formattedEmployerTitle);
+
+		var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+		var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+		var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+		var formattedDatesLocationDesc = formattedDates + formattedLocation + formattedDescription;
+		$(".work-entry:last").append(formattedDatesLocationDesc);
+
+	}
 
 }
-for (job in work.jobs) {
-	$("#workExperience").append(HTMLworkStart);
+// 调用函数
+displayWork();
 
-
-}
+// 添加国际化名称显示按钮
+// 获取鼠标点击时在页面上的坐标
+$(document).click(function(loc) {
+	var x = loc.pageX;
+	var y = loc.pageY;
+	logClicks(x, y);
+});
