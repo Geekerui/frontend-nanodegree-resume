1fbf62e8db453aa4583c13190c256da45763d9ff
Geekerui <gaokangrui@gmail.com>
2017-08-05 00:09:08 +0800

    Delete "Hello World" at index.html; Fixed message of header; Added more information.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,12 +2,13 @@ var bio = {
 	name: "Kangrui",
 	role: "Web Developer",
 	welcomeMessage: "Welcome",
-	bioic: "images/fry.jpg",
+	biopic: "images/fry.jpg",
 	contacts: {
 		mobile: "138-0000-0000",
 		email: "gaokangrui@gmail.com",
 		github: "Geekerui",
 		twitter: "@gaokangrui",
+		blog: "null",
 		location: "Hefei"
 	},
 	skills: ["awesomeness", "delivering things", "cryogenic sleep", "saving the universe"]
@@ -58,7 +59,7 @@ var work = {
 	}]
 };
 var projects = {
-	projects: [{
+	project: [{
 		title: "null",
 		dates: "null",
 		description: "null",
@@ -68,9 +69,32 @@ var projects = {
 
 // 添加一个函数到代码中用来显示之前的内容
 function displayWork() {
+	// 身份
+	var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+	$("#header").prepend(formattedRole);
 	// 显示名字
 	var formattedName = HTMLheaderName.replace("%data%", bio.name);
-	$("#header").append(formattedName);
+	$("#header").prepend(formattedName);
+	// 联系方式
+	var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+	$("#topContacts").append(formattedMobile);
+	var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+	$("#topContacts").append(formattedEmail);
+	var formattedTwiter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+	$("#topContacts").append(formattedTwiter);
+	var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+	$("#topContacts").append(formattedGithub);
+	var formattedBlog = HTMLblog.replace("%data%", bio.contacts.blog);
+	$("#topContacts").append(formattedBlog);
+	var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+	$("#topContacts").append(formattedLocation);
+	// 图片
+	if (bio.biopic.length > 0) {
+		var formattedBiopic = HTMLbioPic.replace("%data%", bio.biopic);
+		$("#header").append(formattedBiopic);
+	}
+	var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+	$("#header").append(formattedWelcomeMsg);
 
 	// 显示技能
 	if (bio.skills.length > 0) {
@@ -86,7 +110,7 @@ function displayWork() {
 		$("#skills").append(formattedSkill);
 	}
 
-	// 显示详情
+	// 显示工作详情
 	for (job in work.jobs) {
 		$("#workExperience").append(HTMLworkStart);
 
@@ -108,9 +132,44 @@ function displayWork() {
 displayWork();
 
 // 添加国际化名称显示按钮
+$("#main").append(internationalizeButton);
+
+// 显示国际化名称
+function inName(nameString) {
+	var nameArray = nameString.split(" ");
+	// 名全部小写
+	nameArray[0] = nameArray[0].toLowerCase();
+	// 姓全部大写
+	nameArray[1] = nameArray[1].toUpperCase();
+	// 分割姓的首字母转为大写，其他保持不变
+	var giveName = nameArray[0].substr(0, 1).toUpperCase() + nameArray[0].substr(1);
+	return giveName + " " + nameArray[1];
+
+}
 // 获取鼠标点击时在页面上的坐标
 $(document).click(function(loc) {
 	var x = loc.pageX;
 	var y = loc.pageY;
 	logClicks(x, y);
 });
+
+// 在 projects 对象中封装 display()函数
+projects.display = function() {
+	for (var i = 0; i < projects.project.length; i++) {
+		$("#projects").append(HTMLprojectStart);
+
+		var formattedTitle = HTMLprojectTitle.replace("%data%", projects.project[i].title);
+		var formattedDates = HTMLprojectDates.replace("%data%", projects.project[i].dates);
+		var formattedDescription = HTMLprojectDescription.replace("%data%", projects.project[i].description);
+		// 添加各个属性到页面中
+		$(".project-entry:last").append(formattedTitle);
+		$(".project-entry:last").append(formattedDates);
+		$(".project-entry:last").append(formattedDescription);
+
+		if (projects.project[i].images.length > 0) {
+			var formattedImage = HTMLprojectImage.replace("%data%", projects.project[i].images);
+			$(".project-entry:last").append(formattedImage);
+		}
+	}
+}
+projects.display();
